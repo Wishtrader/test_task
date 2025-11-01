95a776d579218781022d81e97d9298f4bf5ccdf5
LevyAnisco <lavy@aniscoit.com>
2025-11-01 09:53:10 +0300

    added hero section

--- js/main.js
+++ js/main.js
@@ -1,3 +1,8 @@
+window.addEventListener('load', () => {
+  const navbar = document.querySelector('.navbar');
+  navbar.style.animation = 'navbar-slide-in 0.6s ease-out forwards';
+});
+
 document.querySelector('.navbar__toggle').addEventListener('click', () => {
   document.querySelector('.navbar__menu').classList.toggle('navbar__menu--active');
 });
