ef56002108ea8b7795494f904784360de28330b8
Andrei kamarou <mr.akomarov@gmail.com>
2025-11-01 19:44:15 +0300

    added order section

--- js/main.js
+++ js/main.js
@@ -6,3 +6,10 @@ window.addEventListener('load', () => {
 document.querySelector('.navbar__toggle').addEventListener('click', () => {
   document.querySelector('.navbar__menu').classList.toggle('navbar__menu--active');
 });
+
+document.querySelector('.navbar__link[href="#order__form"]').addEventListener('click', (e) => {
+  e.preventDefault();
+  document.querySelector('#order__form').scrollIntoView({
+    behavior: 'smooth'
+  });
+});
